5028c80d9740cc45adc640a6c2021a9720254833
csf1599 <csfrazier1599@gmail.com>
2024-04-24 10:15:59 -0400

    I am confused because I can only store one line into my file? it is so confusing because I can easily print the result and store the first result, but the subsequent results are just not being stored in the output file.

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include "binaryExpressionTree.h"
 #include "binaryTree.h"
+#include <fstream>
+#include <string>
+
+
 using namespace std;
 
 int main()
 {
-    binaryExpressionTree tree;
-
-    tree.buildExpressionTree(" 3 7 + 14 * ");
-    cout << tree.evaluateExpressionTree() << endl;
+	fstream infile;
+	ofstream ofile;
+	infile.open("RpnData.txt");
+	ofile.open("output.txt");
+	while (!infile.eof()) {
+		binaryExpressionTree tree;
+		string express;
+		getline(infile, express);
+		tree.buildExpressionTree(express);
+		cout << "info being placed into file : " << endl;
+		cout << tree.evaluateExpressionTree() << endl;
+		ofile << to_string(tree.evaluateExpressionTree()) << endl;
+		tree.destroyTree();
+	}
+	ofile.close();
 }
 
 
